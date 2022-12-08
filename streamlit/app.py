c1dd7ba026dc56252f72b0620d929300e100fdaf
avinashm001 <120102387+avinashm001@users.noreply.github.com>
2022-12-08 19:50:58 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import base64
 from PIL import Image
+import base64
 
 st.title("HELLO SEXY")
 st.header("media files")
@@ -12,7 +13,7 @@ This the video source of porn collection :[meganz](https://mega.nz/fm/7lFmQRha)'
 
 
 
-import base64
+
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
@@ -27,5 +28,5 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-add_bg_from_local('car.jpg')  
+add_bg_from_local('t.jpg')  
 
